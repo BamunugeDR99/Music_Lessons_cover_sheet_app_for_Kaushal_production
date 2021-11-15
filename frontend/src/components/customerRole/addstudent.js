c5d466fe613aefec403f4a0c673cc3ea984dc474
sonal-uchiru <sonal123326@gmail.com>
2021-11-15 19:40:52 +0530

    Components divide into 3 sections

---
+++ frontend/src/components/customerRole/addstudent.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function AddStudent(props) {
+  let [name, setName] = useState("");
+  let [age, setAge] = useState("");
+  let [gender, setGender] = useState("");
+
+  function sendData(e) {
+ 
+    e.preventDefault();
+
+    const newStudent = {
+      name,
+      age,
+      gender,
+    };
+
+
+    console.log(newStudent);
+
+    axios
+      .post("http://localhost:8070/student/add", newStudent)
+      .then(() => {
+        alert("Student Added");
+       
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }
+
+  return (
+    <div className="container">
+      <form onSubmit={sendData}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Student Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="age" className="form-label">
+            Student Age
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="age"
+            onChange={(e) => {
+              setAge(e.target.value);
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="gender" className="form-label">
+            Student Gender
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="gender"
+            onChange={(e) => {
+              setGender(e.target.value);
+            }}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+// export default AddStudent;
