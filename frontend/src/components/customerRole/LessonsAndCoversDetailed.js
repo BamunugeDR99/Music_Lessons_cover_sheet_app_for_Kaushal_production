985ffd06a18fb2d8917655f7b28521a5201508e9
sonal-uchiru <sonal123326@gmail.com>
2021-11-16 19:16:23 +0530

    youtuebe embed complete

--- frontend/src/components/customerRole/LessonsAndCoversDetailed.js
+++ frontend/src/components/customerRole/LessonsAndCoversDetailed.js
@@ -98,6 +98,23 @@ export default function LessonsAndCoversDetailed(props) {
                     </span>
                   </a>
                 </div>
+                <br />
+                <h6 style={{ display: "inline", color: "#764A34" }}>
+                  No of pages to preview (free):{" "}
+                </h6>{" "}
+                <h6 style={{ display: "inline" }}>3</h6>
+                <br />
+                <br />
+                <div class="embed-responsive embed-responsive-16by9">
+                  <iframe
+                    class="embed-responsive-item"
+                    src="https://www.youtube.com/embed/CM4CkVFmTds"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                  ></iframe>
+                </div>
               </div>
               <div class="col-sm">One of three columns</div>
             </div>
