b48f4eef29af83e2483832a979779e64b3ca5e5f
sonal-uchiru <sonal123326@gmail.com>
2021-11-16 19:02:34 +0530

    lesson detailed image slider complete

--- frontend/src/components/customerRole/LessonsAndCoversDetailed.js
+++ frontend/src/components/customerRole/LessonsAndCoversDetailed.js
@@ -2,9 +2,108 @@ import React, { useState } from "react";
 import axios from "axios";
 
 export default function LessonsAndCoversDetailed(props) {
-    return(
-        <div>
-            
+  return (
+    <div>
+      <br />
+      <div class="card" style={{ border: "solid #764A34" }}>
+        <div class="card-body">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm">
+                <div
+                  id="carouselExampleIndicators"
+                  class="carousel slide"
+                  data-ride="carousel"
+                >
+                  <ol class="carousel-indicators">
+                    <li
+                      data-target="#carouselExampleIndicators"
+                      data-slide-to="0"
+                      class="active"
+                    ></li>
+                    <li
+                      data-target="#carouselExampleIndicators"
+                      data-slide-to="1"
+                    ></li>
+                    <li
+                      data-target="#carouselExampleIndicators"
+                      data-slide-to="2"
+                    ></li>
+                  </ol>
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img
+                        class="d-block w-100"
+                        src={"/images/923d10247b982186a4ebb24b7ba6fba8.jpg"}
+                        alt="First slide"
+                      />
+                    </div>
+                    <div class="carousel-item">
+                      <img
+                        class="d-block w-100"
+                        src={"/images/923d10247b982186a4ebb24b7ba6fba8.jpg"}
+                        alt="Second slide"
+                      />
+                    </div>
+                    <div class="carousel-item">
+                      <img
+                        class="d-block w-100"
+                        src={"/images/923d10247b982186a4ebb24b7ba6fba8.jpg"}
+                        alt="Third slide"
+                      />
+                    </div>
+                  </div>
+                  <a
+                    class="carousel-control-prev"
+                    href="#carouselExampleIndicators"
+                    role="button"
+                    data-slide="prev"
+                  >
+                    <span style={{ color: "#000000" }} aria-hidden="true">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="60"
+                        height="60"
+                        fill="#000000"
+                        class="bi bi-caret-left-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
+                      </svg>
+                    </span>
+                    <span class="sr-only" style={{ color: "#000000" }}>
+                      Previous
+                    </span>
+                  </a>
+                  <a
+                    class="carousel-control-next"
+                    href="#carouselExampleIndicators"
+                    role="button"
+                    data-slide="next"
+                  >
+                    <span aria-hidden="true" style={{ color: "#000000" }}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="60"
+                        height="60"
+                        fill="#000000"
+                        class="bi bi-caret-right-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+                      </svg>
+                    </span>
+                    <span class="sr-only" style={{ color: "#000000" }}>
+                      Next
+                    </span>
+                  </a>
+                </div>
+              </div>
+              <div class="col-sm">One of three columns</div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
