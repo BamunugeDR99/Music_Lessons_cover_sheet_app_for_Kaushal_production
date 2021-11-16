e9ba4fb95964ab16b3a719c686733562e06b2321
sonal-uchiru <sonal123326@gmail.com>
2021-11-16 20:18:59 +0530

    discover more component created

---
+++ frontend/src/components/customerRole/DicoverMoreCovers.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function DiscoverMoreCovers(props) {
+  return (
+      <div>
+          
+      </div>
+  );
+}
