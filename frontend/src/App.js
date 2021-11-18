a99d830cd52ff753019b47464d3f3c522b719d21
sonal-uchiru <sonal123326@gmail.com>
2021-11-18 13:46:48 +0530

    Completed the fine tune

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,5 @@
 import { render } from "react-dom";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddStudent from "./components/addstudent";
 import "./App.css";
 import "./css/DiscoverMoreCoversStyles.css";
@@ -12,26 +8,26 @@ import DataTableTest from "./components/dataTableTest";
 import CustomerRegistration from "./components/customerRole/CustomerRegistration";
 import CustomerForgotPassword from "./components/customerRole/CustomerForgotPassword";
 import LessonsAndCoversDetailed from "./components/customerRole/LessonsAndCoversDetailed";
-import DiscoverMoreCovers from "./components/customerRole/DicoverMoreCovers";
+import ViewCategories from "./components/adminRole/ViewCategories";
+import BootstrapDataTable from "./components/adminRole/BootstrapDataTable";
+import MainCategoriesDataTable from "./components/adminRole/MainCategoriesDataTable";
 function App() {
   return (
-
-   
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LessonsAndCoversDetailed/>} />
-      <Route path="/a" element={<CustomerRegistration/>} />
-      <Route path="/b" element={<CustomerForgotPassword/>} />
-      {/* <Route path = "/c" element = {<DiscoverMoreCovers/>}/> */}
-      <Route path="/table" element={<DataTableTest/>}/>
-     
-    </Routes>
-  </BrowserRouter>
-    
+      <Routes>
+        <Route path="/" element={<LessonsAndCoversDetailed />} />
+        <Route path="/a" element={<CustomerRegistration />} />
+        <Route path="/b" element={<CustomerForgotPassword />} />
+        {/* <Route path = "/c" element = {<DiscoverMoreCovers/>}/> */}
+        <Route path="/table" element={<DataTableTest />} />
+        <Route path="/d" element={<ViewCategories />} />
+        <Route path="/e" element={<MainCategoriesDataTable />} />
+        {/* <Route path="/e" element={<BootstrapDataTable />} /> */}
 
+      </Routes>
+    </BrowserRouter>
 
-//Commit1
-
+    //Commit1
   );
 }
 
