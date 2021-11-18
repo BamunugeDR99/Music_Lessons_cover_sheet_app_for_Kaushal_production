7ae1bc421b8ef15fa302024c70a51bd2f081e06c
sonal-uchiru <sonal123326@gmail.com>
2021-11-18 12:06:19 +0530

    forgot password visibility completed

--- frontend/src/components/customerRole/CustomerForgotPassword.js
+++ frontend/src/components/customerRole/CustomerForgotPassword.js
@@ -7,12 +7,21 @@ export default function CustomerForgotPassword(props) {
   const [stageThreeStatus, setStageThreeStatus] = useState(true);
   const [lockKeyImage, setLockKeyImage] = useState(false);
   const [unlockKeyImage, setUnLockKeyImage] = useState(true);
+  const [passwordShown, setPasswordShown] = useState(false);
+  const [CpasswordShown, setCPasswordShown] = useState(false);
+  const [eyeSlashIcon, setEyeSlashIcon] = useState(false);
+  const [eyeIcon, setEyeIcon] = useState(true);
+  const [CeyeSlashIcon, setCEyeSlashIcon] = useState(false);
+  const [CeyeIcon, setCEyeIcon] = useState(true);
 
   return (
     <div>
       <br />
       <div className="d-flex justify-content-center">
-        <div class="card" style={{ border: "solid #764A34" }}>
+        <div
+          class="card shadow p-3 mb-5 bg-white rounded"
+          style={{ border: "solid #764A34" }}
+        >
           <div class="card-body">
             <div class="text-center">
               <div hidden={lockKeyImage}>
@@ -169,11 +178,51 @@ export default function CustomerForgotPassword(props) {
                   </span>
                 </span>
                 <input
-                  class="form-control border-left-0"
-                  type="password"
+                  class="form-control border-left-0 border-right-0"
+                  type={passwordShown ? "text" : "password"}
                   id="newPassword"
                   placeholder="Create New Password"
                 />
+                <span class="input-group-append bg-white border-left-0">
+                  <span class="input-group-text bg-transparent">
+                    <div hidden={eyeSlashIcon}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        class="bi bi-eye-slash-fill"
+                        viewBox="0 0 16 16"
+                        onClick={() => {
+                          setPasswordShown(true);
+                          setEyeSlashIcon(true);
+                          setEyeIcon(false);
+                        }}
+                      >
+                        <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z" />
+                        <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z" />
+                      </svg>
+                    </div>
+                    <div hidden={eyeIcon}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        class="bi bi-eye-fill"
+                        viewBox="0 0 16 16"
+                        onClick={() => {
+                          setPasswordShown(false);
+                          setEyeSlashIcon(false);
+                          setEyeIcon(true);
+                        }}
+                      >
+                        <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                        <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+                      </svg>
+                    </div>
+                  </span>
+                </span>
               </div>
               <br />
               <div class="input-group">
@@ -193,11 +242,51 @@ export default function CustomerForgotPassword(props) {
                   </span>
                 </span>
                 <input
-                  class="form-control border-left-0"
-                  type="password"
+                  class="form-control border-left-0 border-right-0"
+                  type={CpasswordShown ? "text" : "password"}
                   id="confirmPassword"
                   placeholder="Confirm Password"
                 />
+                <span class="input-group-append bg-white border-left-0">
+                  <span class="input-group-text bg-transparent">
+                    <div hidden={CeyeSlashIcon}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        class="bi bi-eye-slash-fill"
+                        viewBox="0 0 16 16"
+                        onClick={() => {
+                          setCPasswordShown(true);
+                          setCEyeSlashIcon(true);
+                          setCEyeIcon(false);
+                        }}
+                      >
+                        <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z" />
+                        <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z" />
+                      </svg>
+                    </div>
+                    <div hidden={CeyeIcon}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        class="bi bi-eye-fill"
+                        viewBox="0 0 16 16"
+                        onClick={() => {
+                          setCPasswordShown(false);
+                          setCEyeSlashIcon(false);
+                          setCEyeIcon(true);
+                        }}
+                      >
+                        <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                        <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+                      </svg>
+                    </div>
+                  </span>
+                </span>
               </div>
               <br />
               {/* verify code btn  */}
--- frontend/src/components/customerRole/CustomerRegistration.js
+++ frontend/src/components/customerRole/CustomerRegistration.js
@@ -12,7 +12,7 @@ export default function CustomerRegistration(props) {
     <div>
       <br />
       <div className="d-flex justify-content-center">
-        <div class="card" style={{ border: "solid #764A34" }}>
+        <div class="card shadow p-3 mb-5 bg-white rounded" style={{ border: "solid #764A34" }}>
           <div class="card-body">
             <div class="text-center">
               <img
@@ -213,7 +213,6 @@ export default function CustomerRegistration(props) {
                           fill="currentColor"
                           class="bi bi-lock-fill"
                           viewBox="0 0 16 16"
-                    
                         >
                           <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z" />
                         </svg>
@@ -230,7 +229,7 @@ export default function CustomerRegistration(props) {
                       />
                       <span class="input-group-append bg-white border-left-0">
                         <span class="input-group-text bg-transparent">
-                          <div hidden = {CeyeSlashIcon}>
+                          <div hidden={CeyeSlashIcon}>
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
                               width="20"
@@ -243,13 +242,12 @@ export default function CustomerRegistration(props) {
                                 setCEyeSlashIcon(true);
                                 setCEyeIcon(false);
                               }}
-                          
                             >
                               <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z" />
                               <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z" />
                             </svg>
                           </div>
-                          <div hidden = {CeyeIcon}>
+                          <div hidden={CeyeIcon}>
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
                               width="20"
