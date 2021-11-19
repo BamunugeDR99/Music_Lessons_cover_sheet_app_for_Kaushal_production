58f7884bc8113dc1327f396bdfc73c95d7c4f441
sonal-uchiru <sonal123326@gmail.com>
2021-11-19 12:48:04 +0530

    header updated

--- frontend/src/components/customerRole/CustomerHeader.js
+++ frontend/src/components/customerRole/CustomerHeader.js
@@ -54,11 +54,6 @@ export default function CustomerHeader(props) {
 
         <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
           <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            {/* <li class="nav-item active">
-              <a class="nav-link" href="#">
-                Home <span class="sr-only">(current)</span>
-              </a>
-            </li> */}
             <li class="nav-item">
               <a class="nav-link" href="#" id="classicalHeader">
                 <font>Classical Guitar Covers </font>
@@ -93,10 +88,9 @@ export default function CustomerHeader(props) {
               aria-label="Search"
             />
             <button
-            id = "SearchBtn"
+              id="SearchBtn"
               class="btn  my-2 my-sm-0 btn-sm"
               type="submit"
-              
             >
               Search
             </button>
@@ -118,7 +112,7 @@ export default function CustomerHeader(props) {
                 fontSize: "18px",
                 color: "#764A34",
                 marginRight: "10px",
-                marginLeft : "5px"
+                marginLeft: "5px",
               }}
             >
               My Account{" "}
