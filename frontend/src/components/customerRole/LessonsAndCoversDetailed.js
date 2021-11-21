077f7a83240c4a3c245147f46820ae19b466d102
sonal-uchiru <sonal123326@gmail.com>
2021-11-21 19:38:45 +0530

    image slider incomplete

--- frontend/src/components/customerRole/LessonsAndCoversDetailed.js
+++ frontend/src/components/customerRole/LessonsAndCoversDetailed.js
@@ -1,37 +1,23 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CurrencySelect from "./CurrencySelect";
 import DiscoverMoreCovers from "./DicoverMoreCovers";
 
 export default function LessonsAndCoversDetailed(props) {
-
   const [covers, setCovers] = useState([]);
   let preview = [];
-  let coverDeatils = [];
   let instrumentsTxt = "";
-  let instruments = [];
   useEffect(() => {
     function getCovers() {
       //const CoverID = props.match.params.id;
       const CoverTempID = "61936caa6027859b76b2ddbc";
       axios
-        .get("http://localhost:8070/covers/get/"+ CoverTempID)
+        .get("http://localhost:8070/covers/get/" + CoverTempID)
         .then((res) => {
-
-        
           setCovers(res.data);
           preview = res.data.PreviewPages;
-          instruments = res.data.InstrumentsPlayedOn;
-          for(let i = 0; i < instruments.length; i++){
-            if(instruments.length == 1){
-              instrumentsTxt += instruments[i];
-            }else{
-              instrumentsTxt += instruments[i] + ", ";
-
-            }
-          }
-          document.getElementById("instruments").innerHTML = instrumentsTxt;
-
+          printInstruments(res.data.InstrumentsPlayedOn);
+          displayPreviewImageSlider(res.data.PreviewPages)
         })
         .catch((err) => {
           alert(err);
@@ -40,6 +26,44 @@ export default function LessonsAndCoversDetailed(props) {
 
     getCovers();
   }, []);
+
+  function printInstruments(instruments) {
+    for (let i = 0; i < instruments.length; i++) {
+      if (instruments.length == i + 1) {
+        instrumentsTxt += instruments[i];
+      } else {
+        instrumentsTxt += instruments[i] + ", ";
+      }
+    }
+    document.getElementById("instruments").innerHTML = instrumentsTxt;
+  }
+
+  function displayPreviewImageSlider(previewImages) {
+    let imageSlider = '<div class="carousel-inner">';
+    for (let i = 0; i < previewImages.length; i++) {
+      if(i == 0){
+        let a ='src={"/images/test2.jpg"}';
+        imageSlider += '<div class="carousel-item active"><img id = "img1" class="d-block w-100" alt="slide"/></div>';
+
+        // imageSlider += '<div class="carousel-item active"><img id = "'+"img" +i +'class="d-block w-100" alt="slide"/></div>';
+      }else{
+        //imageSlider += '<div class="carousel-item"><img  id = "'+"img" +i + 'class="d-block w-100" alt="slide"/></div>'; 
+      }
+    }
+    imageSlider += '</div>';
+    document.getElementById("img").innerHTML = imageSlider;
+    for(let i = 0; i < 1;i++){
+      // src={"/images/test2.jpg"}
+      //  let ImagePath = '{"/images/'+ previewImages[i]+ '"}';
+      let ImagePath = "/images/test2.jpg";
+      document.getElementById("img"+(i+1)).src = ImagePath;
+    }
+    
+  
+
+   
+
+  }
   return (
     <div>
       <br />
@@ -69,26 +93,33 @@ export default function LessonsAndCoversDetailed(props) {
                       data-slide-to="2"
                     ></li>
                   </ol>
-                  {/* loop images  */}
-                  <div class="carousel-inner">
-                  <div class="carousel-item active">
-                        <img
-                          class="d-block w-100"
-                          src={"/images/test2.jpg"}
-                          alt="slide"
-                        /></div>
-                    {/* {preview.map((coverPreview,index)=> {
-                      return (
-                        <div class="carousel-item">
-                        <img
-                          class="d-block w-100"
-                          src={"/images/test2.jpg"}
-                          alt="slide"
-                        /></div>
-                      );
-                    })} */}
+                  <div id = "img"></div>
+                  {/* loop images 
+                  {() => {
+                    return (
+                      <div class="carousel-inner">
+                        <div class="carousel-item active">
+                          <img
+                            class="d-block w-100"
+                            src={"/images/test2.jpg"}
+                            alt="slide"
+                          />
+                        </div>
+                        {/* {preview.map((coverPreview, index) => {
+                          return (
+                            <div class="carousel-item">
+                              <img
+                                class="d-block w-100"
+                                src={"/images/test2.jpg"}
+                                alt="slide"
+                              />
+                            </div>
+                          );
+                        })} */}
+                      {/* </div>
+                    ); */}
                   
-                  </div>
+
                   {/* controls  */}
                   <a
                     class="carousel-control-prev"
@@ -139,21 +170,24 @@ export default function LessonsAndCoversDetailed(props) {
                 <h6 style={{ display: "inline", color: "#764A34" }}>
                   No of pages to preview (free):{" "}
                 </h6>{" "}
-                <h6 style={{ display: "inline" }}>{covers.NoOfPreviewPages}</h6><br/>
+                <h6 style={{ display: "inline" }}>{covers.NoOfPreviewPages}</h6>
+                <br />
                 {/* add a facebook icon  */}
-                <h6  style={{ display: "inline", color: "#764A34" }} ><a href = {covers.FacebookLink}>Facebook Link</a></h6>
+                <h6 style={{ display: "inline", color: "#764A34" }}>
+                  <a href={covers.FacebookLink}>Facebook Link</a>
+                </h6>
                 <br />
                 <br />
                 {/* youtube video  */}
                 <div class="embed-responsive embed-responsive-16by9">
                   <iframe
                     class="embed-responsive-item"
-                    // need to use embeded youtube link 
-                    src={covers.YoutubeLink}               
+                    // need to use embeded youtube link
+                    src={covers.YoutubeLink}
                     title="YouTube video player"
-                    frameborder="0"
+                    frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen
+                    allowFullScreen
                   ></iframe>
                 </div>
                 <br />
@@ -161,7 +195,7 @@ export default function LessonsAndCoversDetailed(props) {
               <div class="col-sm">
                 {/* main title */}
                 <h3 style={{ color: "#764A34", letterSpacing: "10px" }}>
-                {covers.Title}
+                  {covers.Title}
                 </h3>
                 <br />
                 {/* original artis name  */}
@@ -181,7 +215,7 @@ export default function LessonsAndCoversDetailed(props) {
                   Arrange by :{" "}
                 </h5>{" "}
                 <h5 style={{ display: "inline", letterSpacing: "2px" }}>
-                {covers.ArrangedBy}
+                  {covers.ArrangedBy}
                 </h5>
                 <br />
                 <br />
@@ -195,9 +229,10 @@ export default function LessonsAndCoversDetailed(props) {
                   {/* instruments played on array  */}
                   Instruments played on :{" "}
                 </h5>{" "}
-                <h5 id = "instruments" style={{ display: "inline", letterSpacing: "2px" }}>
-                  
-                </h5>
+                <h5
+                  id="instruments"
+                  style={{ display: "inline", letterSpacing: "2px" }}
+                ></h5>
                 <br />
                 <br />
                 <h5
@@ -240,14 +275,16 @@ export default function LessonsAndCoversDetailed(props) {
                   {/* no of pages  */}
                   No of pages :{" "}
                 </h5>{" "}
-                <h5 style={{ display: "inline", letterSpacing: "2px" }}>{covers.NoOfPages}</h5>
+                <h5 style={{ display: "inline", letterSpacing: "2px" }}>
+                  {covers.NoOfPages}
+                </h5>
                 <br />
                 <br />
                 <div class="container">
                   <div class="row">
                     <div class="col-sm">
                       {/* calling a another Component */}
-                      <CurrencySelect  coverPrice = {covers.Price}/>
+                      <CurrencySelect coverPrice={covers.Price} />
                       <br />
                       <h3 id="changedValue" style={{ color: "#764A34" }}></h3>
                       {/* spinner  */}
