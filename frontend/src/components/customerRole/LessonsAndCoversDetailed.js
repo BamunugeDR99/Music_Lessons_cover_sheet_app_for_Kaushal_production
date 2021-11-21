d1c3f04a5b1dde50b26127dd7100e253852369b7
sonal-uchiru <sonal123326@gmail.com>
2021-11-21 14:46:26 +0530

    facebook link added

--- frontend/src/components/customerRole/LessonsAndCoversDetailed.js
+++ frontend/src/components/customerRole/LessonsAndCoversDetailed.js
@@ -108,7 +108,8 @@ export default function LessonsAndCoversDetailed(props) {
                   No of pages to preview (free):{" "}
                 </h6>{" "}
                 <h6 style={{ display: "inline" }}>3</h6><br/>
-                {/* <h6  style={{ display: "inline", color: "#764A34" }} >Facebook link : </h6> <h6 style={{ display: "inline" }}><a>https://stackoverflow.com/questions/38518278/how-to-use-jquery-with-reactjs</a></h6> */}
+                {/* add a facebook icon  */}
+                <h6  style={{ display: "inline", color: "#764A34" }} ><a href = "https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_social_media_buttons">Facebook Link</a></h6>
                 <br />
                 <br />
                 {/* youtube video  */}
