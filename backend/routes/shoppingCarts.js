ce4063f2aef328f27df88d2d4d7a5c40b9b6335e
sonal-uchiru <sonal123326@gmail.com>
2021-11-29 13:05:01 +0530

    final merge

--- backend/routes/shoppingCarts.js
+++ backend/routes/shoppingCarts.js
@@ -70,114 +70,110 @@ router.route("/updateCartCovers/:id").put(async (req, res) => {
         .status(500)
         .send({ status: "Error with updating data", error: err.message });
     });
-  
-  
+});
+
+//Get one Customer's Cart
+router.route("/getOneCart/:id").get((req, res) => {
+  let customerID = req.params.id;
+
+  const getOne = Cart.findOne({ CustomerID: customerID }).exec((err, post) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(post);
+    }
+  });
+});
+
+//Delete Cart
+router.route("/deleteCart/:id").delete(async (req, res) => {
+  let CartID = req.params.id;
+
+  await Cart.findByIdAndDelete(CartID)
+    .then(() => {
+      res.status(200).send({ status: "Cart deleted" });
     })
+    .catch((err) => {
+      console.log(err.message);
+      res
+        .status(500)
+        .send({ status: "Error with delete Item", error: err.message });
+    });
+});
 
+//delete cover from cart
+router
+  .route("/deleteCartCover/:coverid/:customerid")
+  .delete(async (req, res) => {
+    let coverid = req.params.coverid;
+    let customerid = req.params.customerid;
 
+    try {
+      const result = await Cart.findOneAndUpdate(
+        { CustomerID: customerid },
+        { $pull: { CoverIDs: coverid } }
+      );
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  });
 
-    
+// db.getCollection('validacao_permalinks').update(
+//   {'pecas.comentarios.id': "c1c586be8ded3d044f96fccc18473cf8"},
+//   {
+//       $pull: { 'pecas.$.comentarios': {
+//           "id":"c1c586be8ded3d044f96fccc18473cf8"}
+//       }
+//    }
+// )
 
+router.route("/updateSItem/:id").put(async (req, res) => {
+  let shoppingID = req.params.id;
+  const { CustomerID, CoverIDs } = req.body;
 
+  const updateItem = {
+    CustomerID,
+    CoverIDs,
+  };
 
-    //Get one Customer's Cart
-    router.route("/getOneCart/:id").get((req, res)=> {
-
-
-        let customerID = req.params.id;
-
-        const getOne = Cart.findOne({CustomerID:customerID}).exec ((err, post) =>{
-
-            if(err){
-
-                console.log(err);
-            }
-
-            else{
-
-                res.send(post);
-            }
-
-        })
-
-
+  const update = await Cart.findByIdAndUpdate(shoppingID, updateItem)
+    .then(() => {
+      res.status(200).send({ status: "cart updated" });
     })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .send({ status: "Error with updating data", error: err.message });
+    });
+});
 
+// router.route("/updateSItem/:id").put(async (req, res) => {
+//     let CartID = req.params.id;
+//     const {
+//       customerID,
+//       ItemIDs,PackageIDs
 
-    //Delete Cart
-    router.route("/deleteCart/:id").delete(async (req, res) => {
+//     } = req.body;
 
-        let CartID = req.params.id;
-    
-        await Cart.findByIdAndDelete(CartID)
-        .then(() => {
-    
-            res.status(200).send({status: "Cart deleted"});
-     
-    
-        }).catch((err) => {
-    
-            console.log(err.message);
-            res.status(500).send({status: "Error with delete Item", error : err.message} );
-        })
-    })
-    
+//     const updateItem = {
+//       customerID,
+//       ItemIDs,
+//       PackageIDs
 
+//     };
 
-    router.route("/updateSItem/:id").put(async (req, res) => {
-        let shoppingID = req.params.id;
-        const {
-          CustomerID,
-          CoverIDs,
-        
-        } = req.body;
-      
-        const updateItem = {
-         CustomerID,
-         CoverIDs
-      
-        };
-      
-        const update = await Cart.findByIdAndUpdate(shoppingID, updateItem)
-          .then(() => {
-            res.status(200).send({ status: "cart updated" });
-          })
-          .catch((err) => {
-            console.log(err);
-            res
-              .status(500)
-              .send({ status: "Error with updating data", error: err.message });
-          });
-      });
-    
-
-
-
-    // router.route("/updateSItem/:id").put(async (req, res) => {
-    //     let CartID = req.params.id;
-    //     const {
-    //       customerID,
-    //       ItemIDs,PackageIDs
-        
-    //     } = req.body;
-      
-    //     const updateItem = {
-    //       customerID,
-    //       ItemIDs,
-    //       PackageIDs
-      
-    //     };
-      
-    //     const update = await Cart.findByIdAndUpdate(CartID, updateItem)
-    //       .then(() => {
-    //         res.status(200).send({ status: "cart updated" });
-    //       })
-    //       .catch((err) => {
-    //         console.log(err);
-    //         res
-    //           .status(500)
-    //           .send({ status: "Error with updating data", error: err.message });
-    //       });
-    //   });
+//     const update = await Cart.findByIdAndUpdate(CartID, updateItem)
+//       .then(() => {
+//         res.status(200).send({ status: "cart updated" });
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//         res
+//           .status(500)
+//           .send({ status: "Error with updating data", error: err.message });
+//       });
+//   });
 
 module.exports = router;
