994f67bd5a1360a6f051b3f687ce1ca3d9d5ba2d
sonal-uchiru <sonal123326@gmail.com>
2021-11-29 12:02:37 +0530

    before merge

--- frontend/src/components/customerRole/LessonsAndCoversDetailed.js
+++ frontend/src/components/customerRole/LessonsAndCoversDetailed.js
@@ -67,7 +67,7 @@ export default function LessonsAndCoversDetailed(props) {
   
     document.getElementById("img").innerHTML = imageSlider;
     for(let i = 0;  i < previewImages.length;i++){
-      document.getElementById("img" + i).src = "/images/Imageplaceholder.png";
+      document.getElementById("img" + i).src = "/images/verticalImageHolder.jpeg";
         
       }
     previewImages.map((previewImage, index) => {
@@ -207,20 +207,20 @@ export default function LessonsAndCoversDetailed(props) {
                 <br />
                 {/* add a facebook icon  */}
                 <div className="text-center">
-                  <a href={covers.FacebookLink}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="50"
-                      height="50"
-                      fill="#3b5998"
-                      class="bi bi-facebook"
-                      viewBox="0 0 16 16"
+                  <a
+                      class="btn rounded"
+                      style={{ backgroundColor: "#3b5998"}}
+                      href={covers.FacebookLink}
+                      role="button"
                     >
-                      <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
-                    </svg>
-                    <br />
-                    Watch it on facebook
-                  </a>
+                      <i
+                        class="fab fa-facebook-f fa-3x"
+                        style={{ color: "#ffffff" }}
+                      ></i>
+                      
+                    </a><br/>
+                   <p style = {{color  : "#3b5998"}}> Watch it on facebook</p>
+
                 </div>
                 <br />
                 <br />
