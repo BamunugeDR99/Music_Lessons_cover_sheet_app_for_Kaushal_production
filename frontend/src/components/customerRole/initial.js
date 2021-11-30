b9322cf7cca371cb297dd70fd6a8879ab7fdd053
kavee <IT20185770@my.sliit.lk>
2021-11-30 19:50:03 +0530

    initial

--- frontend/src/components/customerRole/initial.js
+++ frontend/src/components/customerRole/initial.js
@@ -6,25 +6,24 @@ export default function Initial(props) {
 
     return(
 
-      <div className="intial">
-  
-  {/* <div id="intro" class="bg-image shadow-2-strong">
-      <div class="mask" style={{ backgroundColor: "rgba(0, 0, 0, 0.8)"}}>
-        <div class="container d-flex align-items-center justify-content-center text-center h-100">
-          <div class="text-white">
-            <h1 class="mb-3">Learn Bootstrap 5 with MDB</h1>
-            <h5 class="mb-4">Best & free guide of responsive web design</h5>
-            <a class="btn btn-outline-light btn-lg m-2" href="https://www.youtube.com/watch?v=c9B4TPnak1A" role="button"
-              rel="nofollow" target="_blank">Start tutorial</a>
-            <a class="btn btn-outline-light btn-lg m-2" href="https://mdbootstrap.com/docs/standard/" target="_blank"
-              role="button">Download MDB UI KIT</a>
-          </div>
-        </div>
-      </div>
-    </div> */}
+      <div className="initial">
 
-<div class="bg"></div>
+<div class="hero-image">
+  {/* <div clss="logo">
+  <img src="images/KaushalOfficialLogo.jpeg" alt="logo" class="logo"></img>
+    </div> */}
+  <div class="hero-text">
+    <h1 style={{fontWeight:"bold", fontSize:"36px"}}>KAUSHAL RASHMIKA<br/> PRODUCTIONS</h1>
+  
+    <button type="button" class="btn btn btn-lg" style={{background:"#764A34"}}>GET STARTED</button>
+  </div>
+</div>
 
     </div>
+
+
+
+
+
     )
 }
