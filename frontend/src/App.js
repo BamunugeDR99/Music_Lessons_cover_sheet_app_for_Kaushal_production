b9ba0b404d39f45a7d0177adf56ff4ed64142d8f
sonal-uchiru <sonal123326@gmail.com>
2021-12-01 12:59:46 +0530

    after chamodh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,17 +6,12 @@ import "./css/DiscoverMoreCoversStyles.css";
 import "./css/initial.css";
 import "./css/home.css";
 import React, { Component } from "react";
-import MusicCover from "./components/customerRole/musiccoverpage";
-import MusicCart from "./components/customerRole/music_cart";
-import DashBoard from "./components/adminRole/dashboard";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/c" element={<MusicCover />} />
-        <Route path="/" element={<MusicCart />} />
-        <Route path="/d" element={<DashBoard />} />
       </Routes>
     </BrowserRouter>
   );
